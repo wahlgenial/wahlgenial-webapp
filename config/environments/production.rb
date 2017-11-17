d2a311ee227c9a7c796b11994d474e547ad48ec7
Jan Kus <jan.kus@mac.com>
2017-11-17 14:09:31 +0100

    Implement config for webpacker sass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,10 +24,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
+  config.public_file_server.enabled = true
+  
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
