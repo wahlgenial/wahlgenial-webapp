8bc8d54ce228309b4742a452931b0a7670bfc6f4
Jan Kus <jan.kus@mac.com>
2017-11-22 10:46:14 +0100

    Refactor pages controller and add decorator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
 
 	def home
-		@election_programs = ElectionPrograms::Topic.includes(questions: {opinions: :party})
+		@election_programs = election_programs
+	end
+
+	private
+	def election_programs
+		ElectionPrograms::TopicReactDecorator.decorate_collection(ElectionPrograms::Topic.includes(questions: {opinions: :party}))
 	end
 end
