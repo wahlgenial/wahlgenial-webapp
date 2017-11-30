e66ed4ecb8d0dc21c5cf505df7e59c05eba37b2a
Jan Kus <jan.kus@mac.com>
2017-11-30 19:03:31 +0100

    Http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :http_authenticate
+
+  protected
+  def http_authenticate
+    # This authentication is only for staging server and should be removed in production
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == 'wahl' && password == 'helfer'
+    end
+  end
 end
