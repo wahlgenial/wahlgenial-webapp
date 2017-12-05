c22a0aeccf5f65295d64a506f4fe00df4f2b38bc
Jan Kus <jan.kus@mac.com>
2017-12-05 11:22:51 +0100

    ElectionPrograms storybook clean

--- app/javascript/stories/ElectionProgramsStory.js
+++ app/javascript/stories/ElectionProgramsStory.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import ElectionPrograms from '../components/ElectionPrograms/ElectionPrograms'
-
+import ElectionPrograms from '../components/ElectionPrograms'
+const electionProgramsFixtures = require('../fixtures/election_programs')
 const ElectionProgramsStory = () => {
   return (
-    <div className='my-5'>
-      <ElectionPrograms />
+    <div className='col-lg-6 col-md-12 h-100 mx-auto'>
+      <ElectionPrograms topics={ electionProgramsFixtures.topics } />
     </div>
   )
 }
