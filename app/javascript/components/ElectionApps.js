919d793b49280bbd878240fcc429f752c3285a2c
Jan Kus <jan.kus@mac.com>
2017-12-06 17:06:01 +0100

    ElectionApps ui data fetch

--- app/javascript/components/ElectionApps.js
+++ app/javascript/components/ElectionApps.js
@@ -5,12 +5,32 @@ class ElectionApps extends React.Component {
 
   render () {
     console.log(this.props)
+    const {appsCategories} = this.props
     return (
       <div id='election-apps' className='col-12 bg-warning'>
-        <h1>Hey</h1>
+        {appsCategories.map((category, index) => (
+          <CategoryItem
+            title={ category.title }
+            apps={ category.apps }
+          />
+        ))}
       </div>
     )
   }
+}
+
+const CategoryItem = ({title, apps}) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      {apps.map((app, index) => (
+        <div>
+          <h3>{app.name}</h3>
+          <p>{app.description}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 ElectionApps.defadivtProps = {}
