d82b9347e78ccf03ccba1d919e95576e2942f0e4
Jan Kus <jan.kus@mac.com>
2017-12-06 11:46:46 +0100

    Mount apps image uploader

--- app/models/election_apps/app.rb
+++ app/models/election_apps/app.rb
@@ -1,3 +1,4 @@
 class ElectionApps::App < ApplicationRecord
   belongs_to :category
+  mount_uploader :image, ElectionAppsAppImageUploader
 end
---
+++ app/uploaders/election_apps_app_image_uploader.rb
@@ -0,0 +1,14 @@
+class ElectionAppsAppImageUploader < CarrierWave::Uploader::Base
+
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :thumb do
+    process resize_to_fit: [480, 270]
+  end
+end
