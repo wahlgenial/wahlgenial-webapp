34b4f6e34583f65aa3a2b8d0cf652361b6ea9ca0
Jan Kus <jan.kus@mac.com>
2017-12-07 16:26:08 +0100

    ElectionApps component UI with app select event

--- app/javascript/components/ElectionApps.js
+++ app/javascript/components/ElectionApps.js
@@ -2,17 +2,40 @@ import React from 'react'
 import CategoryItem from './ElectionApps/CategoryItem'
 
 class ElectionApps extends React.Component {
-  state = { selectedApp: null }
+  state = {
+    selectedCategoryIndex: null,
+    selectedAppIndex: null }
 
+  handleAppSelect (categoryIndex, appIndex) {
+    console.log(categoryIndex, appIndex)
+    console.log(this.state)
+    this.setState({
+      selectedCategoryIndex: categoryIndex,
+      selectedAppIndex: appIndex
+    })
+  }
   render () {
-    console.log(this.props)
     const {appsCategories} = this.props
+    const {selectedCategoryIndex, selectedAppIndex} = this.state
+    console.log(selectedCategoryIndex, selectedAppIndex)
+
     return (
       <div id='election-apps' className='d-flex flex-wrap'>
+        <div className='row col-12'>
+          {(selectedCategoryIndex !== null && selectedAppIndex !== null) && (
+            <div className='app-details col-5 row mx-auto'>
+              categoryIndex: {selectedCategoryIndex}<br />
+              selectedAppIndex: {selectedAppIndex}
+            </div>)
+          }
+        </div>
         {appsCategories.map((category, index) => (
-          category.apps.length !== 0 && (<CategoryItem
-            title={ category.title }
-            apps={ category.apps } />)
+          category.apps.length !== 0 && (
+            <CategoryItem
+              categoryIndex={ index }
+              title={ category.title }
+              apps={ category.apps }
+              onClick = { (categoryIndex, appIndex) => { this.handleAppSelect(categoryIndex, appIndex) } } />)
         ))}
       </div>
     )
--- app/javascript/components/ElectionApps/CategoryItem.js
+++ app/javascript/components/ElectionApps/CategoryItem.js
@@ -1,33 +1,44 @@
 import React from 'react'
 
-const CategoryItem = ({title, apps}) => {
+const CategoryItem = ({title, categoryIndex, apps, onClick}) => {
+  const handleClick = (appId) => {
+    onClick(categoryIndex, appId)
+  }
   return (
     <div className='px-4'>
       <h1>{title}</h1>
       <span className='h-100'>
         {apps.map((app, index) => (
           <AppItem
+            onClick={ () => { handleClick(index) } }
             name={ app.name }
-            description={ app.description } />
+            description={ app.description }
+            link={ app.link } />
         ))}
       </span>
     </div>
   )
 }
 
-const AppItem = ({name, description}) => {
+const AppItem = ({name, description, link, onClick}) => {
   const descriptionTurncate = () => {
     return description.substring(0, 82) + ' ...'
+  }
+  const handleClick = (e) => {
+    onClick()
+    e.preventDefault()
   }
   return (
     <div className='app-block card d-inline-block mx-1'>
       <div className="card-body py-4">
         <h4 className="card-title pt-2">{name}</h4>
         <p className="card-text">{descriptionTurncate()}</p>
-        <a href="#">mehr</a>
+        <a href='#' onClick={ (e) => { handleClick(e) } }>
+          mehr
+        </a>
       </div>
-      <div className="card-footer text-center">
-        <a href="#"> Los geht's </a>
+      <div className="card-footer text-center py-2">
+        <a href={ !link ? '#' : link }> Los geht's </a>
       </div>
     </div>
   )
