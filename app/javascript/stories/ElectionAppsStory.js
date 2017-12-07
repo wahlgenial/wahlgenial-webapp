c04ad425fc822146d40cc0f951868020553d96eb
Jan Kus <jan.kus@mac.com>
2017-12-07 09:15:58 +0100

    ElectionApps story

--- app/javascript/stories/ElectionAppsStory.js
+++ app/javascript/stories/ElectionAppsStory.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ElectionApps from '../components/ElectionApps'
-
+const appsCategoriesFixture = require('../fixtures/election_apps')
 const ElectionAppsStory = () => {
   return (
     <div className='col-lg-12 h-100 mt-3'>
-      <ElectionApps />
+      <ElectionApps appsCategories={ appsCategoriesFixture.appsCategories }/>
     </div>
   )
 }
