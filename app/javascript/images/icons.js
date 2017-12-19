505878cba7a1b6f978ef67f8b08f72612d86524d
Jan Kus <jan.kus@mac.com>
2017-12-19 14:57:29 +0100

    Images assets through webpacker

--- app/javascript/images/icons.js
+++ app/javascript/images/icons.js
@@ -4,3 +4,12 @@ export const ElectionAppsCategoriesIcons = {
   infos: require('./icon-app-category-infos.svg'),
   questions: require('./icon-app-category-questions.svg')
 }
+
+export default {
+  menuOpen: require('./icon-menu-open.svg'),
+  menuClose: require('./icon-menu-close.svg'),
+  dropeDown: require('./icon-drop-down.svg'),
+  share: require('./icon-share.svg'),
+  thumbUp: require('./icon-thumb-up.svg'),
+  thumbDown: require('./icon-thumb-down.svg')
+}
