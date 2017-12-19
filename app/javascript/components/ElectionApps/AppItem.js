8d84dedcbe9b1e44c474dfbadec238a754b91bd4
Jan Kus <jan.kus@mac.com>
2017-12-19 17:50:08 +0100

    Mobile version of apps

--- app/javascript/components/ElectionApps/AppItem.js
+++ app/javascript/components/ElectionApps/AppItem.js
@@ -6,32 +6,32 @@ const AppItem = ({name, description, link, selected, onClick}) => {
     return description.substring(0, 98) + ' ...'
   }
   const handleClick = (e) => {
-    if (isDeviceMobile()) {
-      console.log('work on it later')
-    } else {
-      onClick()
-    }
+    if (!isDeviceMobile()) { $('html, body').animate({ scrollTop: 0 }, 300) }
+    onClick()
     e.preventDefault()
-    $('html, body').animate({
-      scrollTop: 0
-    }, 300)
   }
   return (
-    <div className={ 'app-block card d-inline-block mx-1' + (!selected ? '' : ' selected') }>
-      <a href='#app-description'>
-        <div className="card-body text-left py-4" onClick={ (e) => { handleClick(e) } }>
-          <h4 className="card-title pt-2">{name}</h4>
-          <p className="card-text">{descriptionTurncate()}</p>
-          <h5 className='more'>
-            mehr
-          </h5>
+    <div id='AppItem' className='d-inline-block mx-1'>
+      <div className={ !selected ? '' : ' selected' }>
+        <div className='app-block card'>
+          <div className='app-description'>
+            <div className="card-body text-left" onClick={ (e) => { handleClick(e) } }>
+              <h4 className="card-title">{name}</h4>
+              <p className="card-text">{descriptionTurncate()}</p>
+              <h5 className='more'>
+                mehr
+              </h5>
+            </div>
+          </div>
+          <div className='footer-link' href={ !link ? '#' : link }>
+            <a href={ !link ? '#' : link }>
+              <div className="card-footer text-center py-2">
+                 Los geht's
+              </div>
+            </a>
+          </div>
         </div>
-      </a>
-      <a className='footer-link' href={ !link ? '#' : link }>
-        <div className="card-footer text-center py-2">
-           Los geht's
-        </div>
-      </a>
+      </div>
     </div>
   )
 }
