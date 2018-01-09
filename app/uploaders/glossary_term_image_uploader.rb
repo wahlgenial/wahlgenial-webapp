ba2066369c46d3c901a95271bb19cffdf6a18d26
Jan Kus <jan.kus@mac.com>
2018-01-09 10:36:25 +0100

    Terms image quality

--- app/uploaders/glossary_term_image_uploader.rb
+++ app/uploaders/glossary_term_image_uploader.rb
@@ -5,9 +5,10 @@ class GlossaryTermImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
+  process :quality => 100
+  
   version :thumb do
     # https://stackoverflow.com/questions/8570181/carrierwave-resizing-images-to-fixed-width
-    process resize_to_fit: [620 , -1], :quality => 100
+    process resize_to_fit: [620 , -1]
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,15 @@
+module CarrierWave
+  module MiniMagick
+    def quality(percentage)
+      manipulate! do |img|
+        img.quality(percentage)
+        img = yield(img) if block_given?
+        img
+      end
+    end
+  end
+end
+
 CarrierWave.configure do |configure|
 
   if Rails.env.development? || Rails.env.test?
