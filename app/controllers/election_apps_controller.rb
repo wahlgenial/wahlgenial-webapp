58541b37187a168594835dc008af15ea794b9b6d
Jan Kus <jan.kus@mac.com>
2018-02-12 13:20:23 +0100

    Provide user new app controller

--- app/controllers/election_apps_controller.rb
+++ app/controllers/election_apps_controller.rb
@@ -3,9 +3,31 @@ class ElectionAppsController < ApplicationController
     @apps_categories = election_apps_categories
   end
 
+  def new
+    @election_app = ElectionApps::App.new()
+  end
+
+  def create
+    if (@election_app = ElectionApps::App.create(election_app_params))
+      redirect_to new_election_app_path, notice: t('.success')
+    else
+      render :new
+    end
+  end
+
   private
-  
+
   def election_apps_categories
     ElectionApps::CategoryReactDecorator.decorate(ElectionApps::Category.includes(:apps))
+  end
+
+  def election_app_params
+    params.require(:election_apps_app).permit(
+      :name,
+      :description,
+      :image,
+      :category_id,
+      :logo,
+      :link )
   end
 end
