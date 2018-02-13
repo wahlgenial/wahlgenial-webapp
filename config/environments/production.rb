b0f490ba80206dd36b6d7fe265e79755730e7ef8
Jan Kus <jan.kus@mac.com>
2018-02-13 15:43:20 +0100

    :/

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'wahldatenhelfer.heroku.com',
+    :domain         => 'wahldatenhelfer.de',
     :authentication => :plain
   }
 
