0183128da16a2c69946312435415e57207bbcd6a
Jan Kus <jan.kus@mac.com>
2018-02-13 16:28:52 +0100

    Minors specs fixes

--- spec/controllers/election_apps_controller_spec.rb
+++ spec/controllers/election_apps_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ElectionAppsController, type: :controller do
-  # describe "GET #index" do
-  #   it "returns http success" do
-  #     get :index
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 
   describe 'New app' do
     it 'returns http success' do
@@ -24,16 +24,16 @@ RSpec.describe ElectionAppsController, type: :controller do
   end
 end
 
-# RSpec.describe 'Election Apps nested params', type: :request do
-#   describe "Extra Params" do
-#     before do
-#       create(:category, title: 'a Category')
-#     end
-#     it "returns http success" do
-#       get '/wahl-apps/all/those/who/have/made/it'
-#       expect(response).to have_http_status(:success)
-#       expect(controller.instance_variable_get('@apps_categories').inspect).to include('a Category')
-#       expect(controller.params[:params_string]).to eql('all/those/who/have/made/it')
-#     end
-#   end
-# end
+RSpec.describe 'Election Apps nested params', type: :request do
+  describe "Extra Params" do
+    before do
+      create(:category, title: 'a Category')
+    end
+    it "returns http success" do
+      get '/wahl-apps/all/those/who/have/made/it'
+      expect(response).to have_http_status(:success)
+      expect(controller.instance_variable_get('@apps_categories').inspect).to include('a Category')
+      expect(controller.params[:params_string]).to eql('all/those/who/have/made/it')
+    end
+  end
+end
