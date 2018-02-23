950464e0ce155726c4037879fa50c54c60a82d99
Jan Kus <jan.kus@mac.com>
2018-02-23 14:51:13 +0100

    Make all routes german

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  controller :pages do
-    get 'home'
-    get 'imprint'
-    get 'about'
-  end
-
   # matchnig `controller/a/b/c/d....`
   get 'wahl-apps/(*params_string)', to: 'election_apps#index', as: 'apps'
-  get 'glossary/(*params_string)', to: 'glossary#index', as: 'glossary'
+  get 'woerterbuch/(*params_string)', to: 'glossary#index', as: 'glossary'
+  get 'wahl-termine', to: 'calendar#index', as: 'calendar'
 
-  get 'calendar', to: 'calendar#index', as: 'calendar'
   post 'about', to: 'pages#create_app', as: 'new_app'
+
+  get 'home', to: 'pages#home', as: 'home'
+  get 'impressum', to: 'pages#imprint', as: 'imprint'
+  get 'ueber-uns', to: 'pages#about', as: 'about'
+
   root 'pages#home'
 end
