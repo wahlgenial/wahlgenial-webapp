3490d572a404a644dbf3318ac465709918d06f03
Jan Kus <jan.kus@mac.com>
2018-02-26 16:47:12 +0100

    Adjust small-screen for homepage

--- app/javascript/components/HowSectionVideo.js
+++ app/javascript/components/HowSectionVideo.js
@@ -5,16 +5,16 @@ const howCoverImage = require('../images/how-video-cover.jpg')
 const HowSectionVideo = ({children, videoId, startTime = 0}) => {
   return (
     <VideoPlayer videoId={ videoId } startTime={ startTime } coverImage = { howCoverImage }>
-      <div className="how-description col-lg-6 col-md-8 col-sm-12 d-flex text-center">
-        <div className="col-lg-6 col-md-8 col-sm-12 m-auto">
-          <h1 className="py-5">WIE GEHT <span className="color-call-to-action">WÄHLEN?</span></h1>
+      <div className="col-lg-12 d-flex text-center">
+        <div className="how-description d-flex flex-column col-lg-6 col-md-8 col-sm-12 m-auto">
+          <h1>WIE GEHT <span className="color-call-to-action">WÄHLEN?</span></h1>
+          <p>
+            Erststimme, Zweitstimme und Direktmandate? Halb so wild!<br />
+            Was passiert, wenn du deine Stimme abgibst und wie machst du das?<br />
+            In nur zwei Minuten hast du es kapiert.
+          </p>
+          <PlayButton />
         </div>
-        <div className="col-md-10 col-sm-12 m-auto">
-          Erststimme, Zweitstimme und Direktmandate? Halb so wild!<br />
-          Was passiert, wenn du deine Stimme abgibst und wie machst du das?<br />
-          In nur zwei Minuten hast du es kapiert.
-        </div>
-        <PlayButton />
       </div>
     </VideoPlayer>
   )
