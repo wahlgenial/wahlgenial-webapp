c73760bcfa983a1699f8bd0d13796ec36b443a17
Studium <studium@Svens-MacBook-Pro.local>
2019-07-30 17:34:26 +0200

    Add published column to teacher_app data structure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190730125820) do
+ActiveRecord::Schema.define(version: 20190730153237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 20190730125820) do
     t.string "tool"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "published", default: false, null: false
   end
 
   create_table "teacher_apps_tags", force: :cascade do |t|
