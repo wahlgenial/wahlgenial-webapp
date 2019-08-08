3019e154bb6509d340443a69f90459adfe208f6c
Studium <studium@Svens-MacBook-Pro.local>
2019-08-08 14:16:22 +0200

    Drop teacher_apps_tags table

---
+++ db/migrate/20190808105343_drop_teacher_apps_tags_table.rb
@@ -0,0 +1,9 @@
+class DropTeacherAppsTagsTable < ActiveRecord::Migration[5.1]
+  def up
+    drop_table :teacher_apps_tags
+ end
+
+ def down
+   raise ActiveRecord::IrreversibleMigration
+ end
+end
