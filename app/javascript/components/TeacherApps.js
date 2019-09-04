7f0d205a732bcbe924c3fd747a6260a5de4a7cf5
Studium <studium@Svens-MacBook-Pro.local>
2019-09-04 14:39:31 +0200

    Seperate teacher and election apps css

--- app/javascript/components/TeacherApps.js
+++ app/javascript/components/TeacherApps.js
@@ -9,12 +9,16 @@ function TeacherApps({ apps }) {
     }
 
     return (
-
         <div>
             <HeaderLogo />
             <div className="apps-container d-flex flex-wrap justify-content-center">
                 {apps.map(app => {
-                    return <TeacherAppItemDesktop name={app.title} description={app.description} link={app.link} onClick={handleClick(app)} />
+                    return <TeacherAppItemDesktop
+                    name={app.title}
+                    description={app.description} link={app.link}
+                    onClick={handleClick(app)}
+                    tool={app.tool}
+                    tags={app.teacher_app_tags}/>
                 })}
             </div>
         </div>
