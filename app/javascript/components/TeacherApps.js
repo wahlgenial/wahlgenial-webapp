51f15e1053869f6ee5e33fa3d5702705c0c40f02
Cathrin <cpape@smail.th-koeln.de>
2019-09-18 14:00:52 +0200

    MK

--- app/javascript/components/TeacherApps.js
+++ app/javascript/components/TeacherApps.js
@@ -85,40 +85,5 @@ class TeacherApps extends Component {
             </div>
         )
     }
-// =======
-// function TeacherApps({ apps }) {
-//     function tagHandler (app){
-//       var teacherAppsTags = app.teacher_app_tags;
-//       var tagNameString = teacherAppsTags.map(function(val){
-//         return "#" + val.name
-//       }).join(", ")
-//       return tagNameString;
-//     }
-
-//     const handleClick = app => () => {
-//         console.log(apps)
-//     }
-
-
-//     return (
-//         <div>
-//           <div>
-//             <HeaderLogo />
-//           </div>
-//           <div className="apps-container d-flex flex-wrap justify-content-center">
-//               {apps.map(app => {
-//                   return <TeacherAppItemDesktop
-//                   name={app.title}
-//                   teaserText={app.teaser_text}
-//                   link={app.link}
-//                   tool={app.tool}
-//                   tags={tagHandler(app)}
-//                   onClick={handleClick(app)}/>
-//               })}
-//               {apps.length === 0 && <h1>Kein Treffer</h1>}
-//           </div>
-//         </div>
-//     )
-// >>>>>>> 117cb07cdc1bc00a3d257523f5b46242ecfdb5a8
 }
 export default TeacherApps
