e706259e4257df35348f4c7edb136508900907c4
Cathrin <cpape@smail.th-koeln.de>
2019-09-18 12:04:34 +0200

    searchfunction works now on the frontend, design is not ready

--- app/javascript/components/TeacherApps.js
+++ app/javascript/components/TeacherApps.js
@@ -1,29 +1,74 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import HeaderLogo from './HeaderLogo'
 import TeacherAppItemDesktop from './ElectionApps/TeacherAppItemDesktop'
 
 
-function TeacherApps({ apps }) {
-    const handleClick = app => () => {
-        console.log(app)
+class TeacherApps extends Component {
+    state = { searchQuery: "" }
+
+    static propTypes = {
+        apps: PropTypes.array.isRequired
     }
 
-    return (
-        <div>
-            <HeaderLogo />
-            <div className="apps-container d-flex flex-wrap justify-content-center">
-                {apps.map(app => {
-                    return <TeacherAppItemDesktop
-                    name={app.title}
-                    description={app.description} link={app.link}
-                    onClick={handleClick(app)}
-                    tool={app.tool}
-                    tags={app.teacher_app_tags}/>
-                })}
-                {apps.length === 0 && <h1>Kein Treffer</h1>}
+    changeSearchString = event => 
+        this.setState({ searchQuery: event.target.value})        
+
+    handleClick = app => () => console.log(app)
+
+    filterApps = e => {
+        e.preventDefault()
+        
+        console.log(this.state.searchQuery)
+    }
+
+    filteredApps = () => {
+        const { apps } = this.props
+        console.log({apps}, this.state.searchQuery);
+        
+        if (this.state.searchQuery === "") {
+            return apps
+        }
+
+        return apps.filter(app => {
+            
+
+            return app.title.toLowerCase().includes(this.state.searchQuery.toLowerCase()) 
+            || app.description.toLowerCase().includes(this.state.searchQuery.toLowerCase())
+            || app.teaser_text.toLowerCase().includes(this.state.searchQuery.toLowerCase())
+        })
+    }
+
+    render () {
+        const apps = this.filteredApps()
+
+        return (
+            <div>
+            <div className="d-flex flex-wrap justify-content-center my-5">
+                    <h1>Webseiten und Apps <br /> für deinen Politik-Unterricht</h1>
+                </div>
+
+                <div className="d-flex flex-wrap justify-content-center">
+                    <form>
+                        <input name="search" placeholder="Suchbegriff eingeben" className="search-form" onChange={this.changeSearchString}/>
+                        <button onClick={this.filterApps}>Search</button>
+                    </form>
+                </div>
+                <div className="apps-container d-flex flex-wrap justify-content-center">
+                    {apps.map(app => {
+                        return <TeacherAppItemDesktop
+                        name={app.title}
+                        description={app.description} link={app.link}
+                        teaser_text={app.teaser_text} link={app.link}
+                        onClick={this.handleClick(app)}
+                        tool={app.tool}
+                        tags={app.teacher_app_tags}/>
+                    })}
+                    {apps.length === 0 && <h3>Keine Treffer</h3>}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default TeacherApps
