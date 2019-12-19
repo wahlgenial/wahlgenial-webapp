8117ec811d601aea89318fb94a79cb1c9a09d50c
Cathrin <cpape@smail.th-koeln.de>
2019-12-19 10:08:48 +0100

    fix filtering by grade level and implement filter by devices

--- app/javascript/components/TeacherApps.js
+++ app/javascript/components/TeacherApps.js
@@ -8,7 +8,7 @@ class TeacherApps extends Component {
     constructor(props) {
         super(props)
 
-        this.state = { searchQuery: "", selectedGrades: []}
+        this.state = { searchQuery: "", selectedGrades: [], selectedDevices: []}
     }
 
     static propTypes = {
@@ -39,14 +39,29 @@ class TeacherApps extends Component {
     filteredApps = () => {
         const { apps } = this.props
 
-        if (this.state.searchQuery === "" && this.state.selectedGrades.length === 0){
+        if (this.state.searchQuery === "" && this.state.selectedGrades.length === 0 && this.state.selectedDevices.length === 0 ){
             return apps
-        }else {
+        }else{
             //Stufenfilter
-            var appsFiltered = apps.filter(grade => {
-                return this.state.selectedGrades.every(element => grade.school_grade.includes(parseInt(element)))
+            var appsFiltered = apps.filter(app => {
+                console.log("selectedGrades: "+ this.state.selectedGrades) //Grundschule(3-4)
+                console.log(app) //komplette Objekt
+                console.log("grade_for: "+ app.for_oberstufe) //true
+
+                return this.state.selectedGrades.every(selectedGrade => app[selectedGrade] === true)
+                //return this.state.selectedGrades.every(element => grade.school_grade.includes(parseInt(element)))
             })
         }
+        
+            //Geraetefilter
+            appsFiltered = appsFiltered.filter(app => {
+                console.log("selectedGrades: "+ this.state.selectedDevices) //
+                console.log(app) //komplette Objekt
+                console.log("grade_for: "+ app.for_tablet) //
+
+                return this.state.selectedDevices.every(selectedDevice => app[selectedDevice] === true)
+            })
+        
 
         // Wortfilter
         return appsFiltered.filter(app => {
@@ -64,12 +79,33 @@ class TeacherApps extends Component {
         if(e.target.checked){
             this.setState({selectedGrades: [e.target.name, ...this.state.selectedGrades]})
         }else{
-            this.setState({selectedGrades: this.state.selectedGrades.filter(grade => grade !== e.target.name)})
+            this.setState({selectedGrades: this.state.selectedGrades.filter(grade => grade !== e.target.name)});
         }
     }
 
+// Geraetefilter: Speichern der checked Geraet und entfernen der unchecked
+handelDeviceChange = (e) => {
+    console.log(e.target.checked);
+    console.log(e.target.name);
+
+    if(e.target.checked){
+        this.setState({selectedDevices: [e.target.name, ...this.state.selectedDevices]})
+    }else{
+        this.setState({selectedDevices: this.state.selectedDevices.filter(device => device !== e.target.name)});
+    }
+}
+
     render () {
         const apps = this.filteredApps()
+        const filterGrades = [{name: "for_grundschule", label: "Grundschule (3-4)"},
+                              {name: "for_unterstufe", label: "Unterstufe (5-7)"}, 
+                              {name: "for_mittelstufe", label: "Mittelstufe (8-9)"}, 
+                              {name: "for_oberstufe", label: "Oberstufe (10-13)"}]
+
+        const filterDevices = [{name: "for_smartphone", label: "Smartphone"},
+                              {name: "for_tablet", label: "Tablet"}, 
+                              {name: "for_desktop", label: "Computer"}, 
+                              {name: "for_whiteboard", label: "Whiteboard"}]
         return (
 
             <div>
@@ -77,7 +113,8 @@ class TeacherApps extends Component {
 
                 <div className="teacher-content">
                     <div className="teacher-heading">
-                        <h1>Webseiten und Apps <br/> für deinen Politik-Unterricht</h1>
+                        <h1>Politik Apps &amp; Tools</h1>
+                        <h4>Diese Apps und Angebote helfen Lehrern ihre Schüler rund um das Thema Wahlen in ihrem Politik-Unterricht zu informieren.  </h4>
                     </div>
 
                     {/* Hier wird das Suchfeld erstellt */}
@@ -87,21 +124,42 @@ class TeacherApps extends Component {
                         </form>
                     </div>
 
-                  {/* Hier werden die controlled Componentes/Forms React (checkboxen) erstellt */}
+                  {/* Hier werden die controlled Componentes/Forms React (checkboxen) erstellt fuer die Klassenstufen */}
                   <div className="grade-container">
                     <form id = "klassen">
                       <div className="grade-container">
-                        <h8>Stufe: </h8>
-                          {["3","4","5","6","7","8","9","10","11","12","13"].map(grade =>{
-                            return <div key={grade} className="grades">
+                        <h8>STUFE </h8>
+                          {filterGrades.map(grade =>{
+                            return <div key={grade.name} className="grades">
                               <input
                                className="grade-one"
                                type="checkbox"
-                               name={grade}
+                               name={grade.name}
                                onChange={this.handelGradeChange}
-                               checked={this.state.selectedGrades.includes(grade)}
-                               id={grade}/>
-                              <label htmlFor={grade}>{grade}</label>
+                               checked={this.state.selectedGrades.includes(grade.name)}
+                               id={grade.name}/>
+                              <label htmlFor={grade.label}>{grade.label}</label>
+                            </div>
+                          })}
+                        </div>
+                      </form>
+                    </div>
+
+                {/* Hier werden die controlled Componentes/Forms React (checkboxen) erstellt fuer die Geraete */}
+                  <div className="grade-container">
+                    <form id = "klassen">
+                      <div className="grade-container">
+                        <h8>GERÄTE </h8>
+                          {filterDevices.map(tool =>{
+                            return <div key={tool.name} className="tools">
+                              <input
+                               className="grade-one"
+                               type="checkbox"
+                               name={tool.name}
+                               onChange={this.handelDeviceChange}
+                               checked={this.state.selectedDevices.includes(tool.name)}
+                               id={tool.name}/>
+                              <label htmlFor={tool.label}>{tool.label}</label>
                             </div>
                           })}
                         </div>
